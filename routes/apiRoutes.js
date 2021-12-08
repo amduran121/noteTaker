714638d86db9f803b2b9962ac1eb7a2567866dfc
Austin Duran <amduran121@gmail.com>
2021-12-08 15:57:50 -0700

    oof

---
+++ routes/apiRoutes.js
@@ -0,0 +1,22 @@
+const router = require ("express").Router()
+const store = require ("../db/store")
+router.get("/notes", (req, res) => {
+    store
+    .getNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err))
+    })
+router.post("/notes", (req, res) => {
+    store
+    .addNote(req.body)
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err))
+
+})
+router.delete("/notes/:id", (req, res) => {
+    store
+    .deleteNote(req.params.id)
+    .then(notes => res.json({ok:true}))
+    .catch(err => res.status(500).json(err))
+})
+module.exports = router
